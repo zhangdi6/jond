541a53c22be65e0d012f6e13099206ba1e4c43d8
Jond <adipay@163.com>
2026-05-21 22:00:19 +0800

    修改内容

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const projects = [
     subtitle: "轻量简约",
     description:
       "小清新的倾诉树洞应用",
-    tags: ["Flutter"，"Kotlin"],
+    tags: ["Flutter", "Kotlin"],
     stores: [
       { label: "App Store", url: "https://apps.apple.com/app/id1588053931" },
       { label: "应用宝", url: "https://a.app.qq.com/o/simple.jsp?pkgname=com.qiyuan.sky" },
@@ -31,7 +31,7 @@ const projects = [
       "一款线下社交应用，让社交回到现实，让关系自然发生。 潮向，由你而定。",
     tags: ["iOS", "Android", "Web"],
     stores: [
-      { label: "Mac App Store", url: "https://apps.apple.com/app/id6483942038" },
+      { label: "App Store", url: "https://apps.apple.com/app/id6483942038" },
       { label: "应用宝", url: "https://a.app.qq.com/o/simple.jsp?pkgname=ad.anhao.uni" },
     ],
   },
